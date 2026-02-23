30411b376f19a54fc19a31e7761e77700dcb437b
Yian Goh <yian.goh@kroo.com>
2026-02-23 12:28:57 +0000

    [CLOUD-867] Change deprecated argument

--- nat.tf
+++ nat.tf
@@ -1,11 +1,11 @@
 resource "aws_eip" "nat" {
   count = local.include_nat_gateways == "yes" ? length(var.availability_zones) : 0
 
-  vpc = true
+  domain = "vpc"
 
   tags = {
-    Name = "eip-nat-${var.component}-${var.deployment_identifier}-${element(var.availability_zones, count.index)}"
-    Component = var.component
+    Name                 = "eip-nat-${var.component}-${var.deployment_identifier}-${element(var.availability_zones, count.index)}"
+    Component            = var.component
     DeploymentIdentifier = var.deployment_identifier
   }
 }
@@ -14,15 +14,15 @@ resource "aws_nat_gateway" "base" {
   count = local.include_nat_gateways == "yes" ? length(var.availability_zones) : 0
 
   allocation_id = element(aws_eip.nat.*.id, count.index)
-  subnet_id = element(aws_subnet.public.*.id, count.index)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
 
   depends_on = [
     aws_internet_gateway.base_igw
   ]
 
   tags = {
-    Name = "nat-${var.component}-${var.deployment_identifier}-${element(var.availability_zones, count.index)}"
-    Component = var.component
+    Name                 = "nat-${var.component}-${var.deployment_identifier}-${element(var.availability_zones, count.index)}"
+    Component            = var.component
     DeploymentIdentifier = var.deployment_identifier
   }
 }
